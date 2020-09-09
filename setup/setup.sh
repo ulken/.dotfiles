42d6244ac6defc4a212f336c96a07a92343e6fbf
Oskar Löfgren <islander.abroad@gmail.com>
2020-09-09 13:26:06 +0200

    :hammer: Add user defaults to setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,9 +5,10 @@ set -e
 
 # check for necessary prerequisites
 if ! xcode-select -p >/dev/null 2>&1; then
-    echo "Xcode Command Line Tools required, please run: `xcode-select --install`"
+    echo "Xcode Command Line Tools required, please run: \`xcode-select --install\`"
     exit 2 # missing command
 fi
 
 ./brew.sh
 ./post_install.sh
+./user_defaults.sh
