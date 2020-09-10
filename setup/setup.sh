df788414ec9a8d323aaacc3db2981e980415a177
Oskar Löfgren <islander.abroad@gmail.com>
2020-09-10 22:55:02 +0200

    :art: Convert from 4 to 2 spaces

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,21 +3,21 @@
 set -e
 
 function install_xcode_cli_tools() {
-    local lock_path=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+  local lock_path=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 
-    product=$(softwareupdate -l |
-        grep "\*.*Command Line" |
-        head -n 1 | awk -F"*" '{print $2}' |
-        sed -e 's/^ *//' |
-        tr -d '\n')
-    softwareupdate -i "$product" --verbose
+  product=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+  softwareupdate -i "$product" --verbose
 
-    rm $lock_path
+  rm $lock_path
 }
 
 if xcode-select -p > /dev/null 2>&1; then
-    echo "Xcode Command Line Tools required, installing..."
-    install_xcode_cli_tools
+  echo "Xcode Command Line Tools required, installing..."
+  install_xcode_cli_tools
 fi
 
 ./brew.sh
