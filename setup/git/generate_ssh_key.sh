9e52d0414be3c9cb8f199d6122d9054986e3f213
Oskar Löfgren <islander.abroad@gmail.com>
2020-09-10 16:31:16 +0200

    :bug: Fix copying public SSH key

--- setup/git/generate_ssh_key.sh
+++ setup/git/generate_ssh_key.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 email="$1"
+key="${2:-id_rsa}"
 
 if [ -z "$email" ]; then
-	email="$(git config --global user.email)"
+  email="$(git config --global user.email)"
 fi
 
-ssh-keygen -t rsa -b 4096 -C "$email"
+ssh-keygen -t rsa -b 4096 -C "$email" -f "$HOME/.ssh/$key"
 
 pbcopy < "$HOME/.ssh/$key.pub"
 echo "Public SSH key copied to clipboard!"
