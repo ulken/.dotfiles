a9812584963db2eb333bc4aec80f4465590766c7
Oskar Löfgren <islander.abroad@gmail.com>
2020-09-11 23:26:36 +0200

    :bug: Allow `brew_updater` to be installed from any path

--- setup/brew_updater/install.sh
+++ setup/brew_updater/install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-PROPERTY_LIST_FILE="com.ulknest.brew-update.plist"
+SOURCE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROPERTY_LIST_PATH="$SOURCE_PATH/com.ulknest.brew-update.plist"
 
-cp "./$PROPERTY_LIST_FILE" "$HOME/Library/LaunchAgents/"
+cp "$PROPERTY_LIST_PATH" "$HOME/Library/LaunchAgents/"
 
-launchctl load "$PROPERTY_LIST_FILE"
+launchctl load "$PROPERTY_LIST_PATH"
