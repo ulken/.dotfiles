b095194718a335c2bc69b8f512b2e28017e97e22
Oskar Löfgren <islander.abroad@gmail.com>
2020-11-22 22:38:16 +0100

    :sparkles: Add `pw` for copying password to clipboard

--- .functions.zsh
+++ .functions.zsh
@@ -1,4 +1,4 @@
-function streamshell() {
+streamshell() {
     local command
     local arguments
     local seashells_options=()
@@ -17,7 +17,17 @@ function streamshell() {
     command=$1
     shift
 
-    arguments=$*
+    $command "$@" 2>&1 | seashells $seashells_options
+}
 
-    $command "$arguments" 2>&1 | seashells $seashells_options
+pw() {
+    local -hr path="$1"
+
+    # workaround for password prompt not working in subshell/pipe
+    # this will trigger password prompt if needed,
+    # otherwise just print an empty line
+    ph --no-cache grep "DONT_MATCH"
+
+    ph show --field=password "$path" | pbcopy
+    echo "Password copied to clipboard"
 }
