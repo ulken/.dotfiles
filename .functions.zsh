a1578b7cd8e37a745921ef7addf51a8de36b85f1
Oskar Löfgren <islander.abroad@gmail.com>
2022-03-04 12:03:06 +0100

    :fire: Remove `streamshell()`
    seashells.io has been discontinued. Replaced by shellshare.net.

--- .functions.zsh
+++ .functions.zsh
@@ -1,25 +1,3 @@
-streamshell() {
-    local command
-    local arguments
-    local seashells_options=()
-
-    if [[ "$*" == *"--"* ]]; then
-        while [[ $1 != "--" ]] || [[ $# == 0 ]]; do
-            seashells_options+=$1
-            shift
-        done
-
-        if [[ $1 == "--" ]]; then
-            shift
-        fi
-    fi
-
-    command=$1
-    shift
-
-    $command "$@" 2>&1 | seashells $seashells_options
-}
-
 pw() {
     set -o pipefail
 
