8fc6e56d3dfdda2c5f378f47916751421b6ac5a4
Oskar Löfgren <516549+ulken@users.noreply.github.com>
2023-04-14 10:38:48 +0200

    :wrench: Add Copilot CLI aliases

--- .aliases.zsh
+++ .aliases.zsh
@@ -44,3 +44,7 @@ alias ungron="gron --ungron"
 alias pc="klog create --should=6h25m" # punch clock
 alias pr="klog today --diff --now | head -2 | tail -n 1 | choose -1" # punch time remaining
 alias pe="klog edit" # punch edit
+
+# copilot
+eval "$(github-copilot-cli alias -- "$0")"
+
