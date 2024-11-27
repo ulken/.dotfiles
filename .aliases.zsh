3d344ab147a24a0ca54862d9e252c54c7ed51240
Oskar Löfgren <516549+ulken@users.noreply.github.com>
2024-11-27 11:36:37 +0100

    simplify pr alias, rremove choose dependency

--- .aliases.zsh
+++ .aliases.zsh
@@ -40,5 +40,5 @@ alias ungron="gron --ungron"
 
 # klog
 alias pc="klog create"                                               # punch clock
-alias pr="klog today --diff --now | head -2 | tail -n 1 | choose -1" # punch time remaining
+alias pr="klog today --diff --now | head -2 | tail -n 1" # punch time remaining
 alias pe="klog edit"                                                 # punch edit
